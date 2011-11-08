f2cb550f6363e101cc3154357d6c976278763bea
gulendol@gmail.com <gulendol@gmail.com@497d2ba7-c3dc-355f-c118-b8d760c8ff58>
2011-11-08 04:11:09 +0000

    2.0 개발 중

---
+++ trunk/main/java/org/jhlabs/scany/service/AbstractAnyService.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.jhlabs.scany.service;
+
+import org.jhlabs.scany.context.rule.BaseServiceRule;
+import org.jhlabs.scany.engine.search.AnySearcher;
+
+/**
+ *
+ * @author Gulendol
+ *
+ * <p>Created: 2011. 11. 6. 오후 4:46:36</p>
+ *
+ */
+public abstract class AbstractAnyService {
+	
+	public abstract AnyTransaction getTransaction(String tableId);
+	
+	public abstract AnySearcher getSercher(String tableId);
+	
+}
