64dd1bbfa6e1ee4d0a59bd1fdf8d1f7b308d6270
Gulendol <Gulendol@497d2ba7-c3dc-355f-c118-b8d760c8ff58>
2011-11-29 16:10:52 +0000

    2.0 개발 중

---
+++ main/java/org/jhlabs/scany/engine/analysis/kr/dic/DicFileReader.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.jhlabs.scany.engine.analysis.kr.dic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+/**
+ *
+ * @author Gulendol
+ *
+ * <p>Created: 2011. 11. 29. 오후 10:06:43</p>
+ *
+ */
+public class DicFileReader {
+
+	BufferedReader br = null;
+
+	public DicFileReader(String fileName) throws UnsupportedEncodingException {
+		this.br = new BufferedReader(new InputStreamReader(super.getClass().getResourceAsStream(fileName), "UTF-8"));
+	}
+
+	public String readLine() throws IOException {
+		return this.br.readLine();
+	}
+
+	public void cleanup() throws IOException {
+		if(this.br == null)
+			return;
+		this.br.close();
+	}
+}
